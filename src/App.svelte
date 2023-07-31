39a87c5b02b1953c044c983344e08ab0e3e69b07
Jonster5 <onlyaccelerating@gmail.com>
2023-07-31 15:52:29 -0400

    yup

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { ECS } from 'raxis';
-	import { CanvasSettings, KeysToTrack, defaultPlugins } from 'raxis-plugins';
+	import { ECS, Vec2 } from 'raxis';
+	import {
+		CanvasSettings,
+		KeysToTrack,
+		Sprite,
+		Transform,
+		defaultPlugins,
+	} from 'raxis-plugins';
 
 	let target: HTMLElement;
 
-	const ecs = new ECS().insertPlugins(...defaultPlugins);
+	const ecs = new ECS()
+		.insertPlugins(...defaultPlugins)
+		.addStartupSystem((ecs) => {
+			const transform = new Transform(new Vec2(100, 100));
+			transform.avel = Math.PI / 4;
+
+			ecs.spawn(transform, new Sprite('rectangle', 'blue'));
+		});
 
 	onMount(() => {
 		ecs.insertResource(
@@ -25,12 +38,18 @@
 
 <style lang="scss">
 	main {
+		display: flex;
 		width: 100%;
 		height: 100%;
+
+		justify-content: center;
+		align-items: center;
 
 		div {
 			width: 60%;
 			height: 60%;
+			background: whitesmoke;
+			border: 3px solid black;
 		}
 	}
 </style>
