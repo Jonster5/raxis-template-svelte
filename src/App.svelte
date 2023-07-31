7d70501845b14da5a988c2800f76e38302a0e72a
Jonster5 <onlyaccelerating@gmail.com>
2023-07-31 15:45:35 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { ECS } from 'raxis';
+	import { CanvasSettings, KeysToTrack, defaultPlugins } from 'raxis-plugins';
+
+	let target: HTMLElement;
+
+	const ecs = new ECS().insertPlugins(...defaultPlugins);
+
+	onMount(() => {
+		ecs.insertResource(
+			new CanvasSettings({
+				target,
+				width: 2000,
+			})
+		)
+			.insertResource(new KeysToTrack([]))
+			.run();
+	});
+</script>
+
+<main>
+	<div bind:this={target} />
+</main>
+
+<style lang="scss">
+	main {
+		width: 100%;
+		height: 100%;
+
+		div {
+			width: 60%;
+			height: 60%;
+		}
+	}
+</style>
